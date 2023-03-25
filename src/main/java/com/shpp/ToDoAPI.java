75720a846da03f927e0079c93204796b4115976f
Maksimus3615 <olkhovskiy.nkmz@gmail.com>
2023-03-26 01:54:42 +0200

    rename API

--- src/main/java/com/shpp/ToDoAPI.java
+++ src/main/java/com/shpp/ToDoAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Task74Application {
+public class ToDoAPI {
 
     public static void main(String[] args) {
-        SpringApplication.run(Task74Application.class, args);
+        SpringApplication.run(ToDoAPI.class, args);
     }
 
 }
